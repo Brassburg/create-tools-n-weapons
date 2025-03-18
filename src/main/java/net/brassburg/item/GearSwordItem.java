1662b740d8dedcf70bb6f5597eb3f91afbcfeb56
Tur87 <piffenedwall@hotmail.com>
2025-03-18 18:55:34 +0100

    source code

---
+++ src/main/java/net/brassburg/item/GearSwordItem.java
@@ -0,0 +1,53 @@
+
+package net.brassburg.item;
+
+import net.minecraft.world.level.Level;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.SwordItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+import net.minecraft.network.chat.Component;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+
+import net.brassburg.init.CreateToolsNWeaponsModTabs;
+
+import java.util.List;
+
+public class GearSwordItem extends SwordItem {
+	public GearSwordItem() {
+		super(new Tier() {
+			public int getUses() {
+				return 2500;
+			}
+
+			public float getSpeed() {
+				return 14f;
+			}
+
+			public float getAttackDamageBonus() {
+				return 8f;
+			}
+
+			public int getLevel() {
+				return 0;
+			}
+
+			public int getEnchantmentValue() {
+				return 22;
+			}
+
+			public Ingredient getRepairIngredient() {
+				return Ingredient.of();
+			}
+		}, 3, -2f, new Item.Properties().fireResistant());
+		ItemGroupEvents.modifyEntriesEvent(CreateToolsNWeaponsModTabs.TAB_CREATE_TOOLS_N_WEAPONS).register(content -> content.accept(this));
+	}
+
+	@Override
+	public void appendHoverText(ItemStack itemstack, Level world, List<Component> list, TooltipFlag flag) {
+		super.appendHoverText(itemstack, world, list, flag);
+	}
+}
